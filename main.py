5d3368a7e5d3abed09eb3031157b0f4472732462
Kirill Iizumskii <mr.llirus@gmail.com>
2020-07-23 23:24:06 +0300

    Import. Module. Package

---
+++ main.py
@@ -0,0 +1,8 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+import datetime
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
+    print(datetime.datetime.now())
